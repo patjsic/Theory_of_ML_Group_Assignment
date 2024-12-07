937b05e2fe181e2cc66cdf47bbcf00221a1f4cf5
Patrick Sicurello <43273424+patjsic@users.noreply.github.com>
2024-12-06 22:41:26 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def create_sequences(data, sequence_length):
-    sequences = []
-    for i in range(len(data) - sequence_length):
-        seq = data[i:i+sequence_length]
-        sequences.append(seq)
-    return torch.tensor(sequences, dtype=torch.float32)
